556b7eedd140277c62f2bba4d0e31ed83a6afe53
Will Schlitzer <schlitzer90@gmail.com>
2023-12-07 17:53:49 -0500

    Add Day 1 Part 1

---
+++ day1/day1_pt1.py
@@ -0,0 +1,22 @@
+calibration_input_file = "test_input1.txt"
+
+calibration_values = 0
+
+calibration_value_list = []
+
+with open(calibration_input_file, mode="r") as file:
+    for line in file.readlines():
+        line_nums = []
+        for char in line:
+            if char.isdigit():
+                line_nums.append(str(char))
+        if len(line_nums)<2:
+            line_nums.append(line_nums[0])
+        calibration_value = int(line_nums[0] + line_nums[-1])
+        calibration_value_list.append(calibration_value)
+
+#print(calibration_value_list)
+for value in calibration_value_list:
+    calibration_values += value
+
+print(calibration_values)
