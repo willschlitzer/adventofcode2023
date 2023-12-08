0eb2161c5af86ad1e1e700e0b794f09760b4ac04
Will Schlitzer <schlitzer90@gmail.com>
2023-12-08 15:53:13 -0500

    complete day1_pt2.py

--- day1/day1_pt2.py
+++ day1/day1_pt2.py
@@ -1,4 +1,4 @@
-calibration_input_file = "example_input2.txt"
+calibration_input_file = "test_input1.txt"
 
 calibration_values = 0
 
@@ -8,6 +8,7 @@ letter_num_dict = {"one":1, "two":2, "three": 3, "four": 4, "five": 5, "six": 6,
 
 with open(calibration_input_file, mode="r") as file:
     for line in file.readlines():
+        #print(line)
         line_nums = []
         for i,char in enumerate(line):
             if char.isdigit():
@@ -15,13 +16,15 @@ with open(calibration_input_file, mode="r") as file:
             else:
                 for key in letter_num_dict:
                     try:
-                        new_string = line[i:i+1+len(key)]
+                        new_string = line[i:i+len(key)]
+                        #print(new_string)
                         if new_string == key:
                             line_nums.append(str(letter_num_dict[key]))
                     except IndexError:
                         pass
         if len(line_nums)<2:
             line_nums.append(line_nums[0])
+        #print(line_nums)
         calibration_value = int(line_nums[0] + line_nums[-1])
         calibration_value_list.append(calibration_value)
 
