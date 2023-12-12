164b268987577987e3aea5be22667a44b6927a41
Will Schlitzer <schlitzer90@gmail.com>
2023-12-12 04:46:19 -0500

    run black on day11_pt1.py

--- day11/day11_pt1.py
+++ day11/day11_pt1.py
@@ -5,24 +5,24 @@ data = example_string.split()
 
 empty_line_index_list = []
 
-for i,line in enumerate(data):
+for i, line in enumerate(data):
     if "#" not in line:
         empty_line = line
         empty_line_index_list.append(i)
 
-#print(empty_line_index_list)
-#print(empty_line_index_list[-1::-1])
+# print(empty_line_index_list)
+# print(empty_line_index_list[-1::-1])
 for i in empty_line_index_list[-1::-1]:
     data.insert(i, empty_line)
 
-#print(data)
+# print(data)
 
 distance_lists = []
 
 galaxy_list = []
 
-for iy,line in enumerate(data):
-    for ix,char in enumerate(line):
+for iy, line in enumerate(data):
+    for ix, char in enumerate(line):
         if char == "#":
             galaxy_list.append([ix, iy])
 
@@ -34,11 +34,10 @@ for _ in range(9):
     galaxy_distance_list = []
     galaxy = galaxy_list.pop(0)
     for galaxy_2 in galaxy_list:
-        dx = abs(galaxy_2[0]-galaxy[0]+1)
-        dy = abs(galaxy_2[1]-galaxy[1]+1)
+        dx = abs(galaxy_2[0] - galaxy[0] + 1)
+        dy = abs(galaxy_2[1] - galaxy[1] + 1)
         galaxy_distance_list.append(dx + dy)
     print(galaxy_distance_list)
-    
 
     total_distance += sum(galaxy_distance_list)
 
